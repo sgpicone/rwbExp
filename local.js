7d79ba6236b602d3767a098ae527102ba983bb7d
Boatmad <themaddestboat@gmail.com>
2020-10-03 20:33:11 -0400

    getting it ready for sharing and caring

--- local.js
+++ local.js
@@ -1,6 +1,9 @@
-const app = require('./src/server.js');
 const port = process.env.PORT || 8000;
 
+
+const app = require('./src/server.js');
+
+console.log(process.env.DB_HOST);
 // Server
 app.listen(port, () => {
    console.log(`Listening on: http://localhost:${port}`);
---
+++ src/dbFactory.js
@@ -0,0 +1,18 @@
+const util = require( 'util' );
+const mysql = require( 'mysql' );
+
+function makeDb( config ) {
+  const connection = mysql.createConnection( config );  return {
+    query( sql, args ) {
+      return util.promisify( connection.query )
+        .call( connection, sql, args );
+    },
+    close() {
+      return util.promisify( connection.end ).call( connection );
+    }
+  };
+}
+
+module.exports = {
+    makeDb: makeDb
+};
